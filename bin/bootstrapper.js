1aab7db34bf236454fd1498f647d092b23c6df07
Elijah Daniel <elijahdanie@gmail.com>
2024-04-20 16:24:20 +0100

    Update package dependencies and versions

--- bin/bootstrapper.js
+++ bin/bootstrapper.js
@@ -59,8 +59,8 @@ const createEndpoint = (controller, endpoint) => {
     const repository = `${controller}Repository`;
 
     return`
-import {LRPCAuth, LRPCFunction, LRPCPayload } from "mrn-lrpc";
-import { BaseResponse, HandlerConfig, LRPCRequest, Status, IEndpoint } from "mrn-lrpc";
+import {LRPCAuth, LRPCFunction, LRPCPayload } from "@elijahdanie/lrpc";
+import { BaseResponse, HandlerConfig, LRPCRequest, Status, IEndpoint } from "@elijahdanie/lrpc";
 import ${repository} from "../${repository}";
 import Container, {Service} from "typedi";
 
