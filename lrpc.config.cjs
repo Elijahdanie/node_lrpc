bf79f7dd68874495bf518d885405fd73aca2d99c
Elijah Daniel <elijahdanie@gmail.com>
2025-03-11 10:04:34 +0100

    update

--- lrpc.config.cjs
+++
@@ -1,8 +0,0 @@
-
-    module.exports = {
-        secret: 'some-secret-key',
-        clientDir: './src/clients',
-        appSecret: 'some-secret',
-        serverUri: 'https://apigateway-dev.smartbloksai.com/client',
-    }
-    
